ed4ef375176ae37819ad57bc2c3f7c5558ba61da
Rio Pacit <rio.pacit@wework.com>
2018-02-13 17:26:52 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,4 +6,6 @@ def display_rainbow(colors)
     puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
-puts display_rainbow(colors)
+puts "The first letter in the alphabet is: #{colors[4]}"
+puts "The second letter in the alphabet is: #{colorss[3]}"
+puts "The third letter in the alphabet is: #{colors[5]}"
