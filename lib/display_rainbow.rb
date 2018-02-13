cca75326370ff564371a9fd9b87f35e364a24b96
Rio Pacit <rio.pacit@wework.com>
2018-02-13 17:30:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,4 +6,10 @@ def display_rainbow(colors)
     puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
-puts display_rainbow(colors)
+puts "The first letter in the alphabet is: #{colors[4]}"
+puts "The second letter in the alphabet is: #{colors[3]}"
+puts "The third letter in the alphabet is: #{colors[5]}"
+puts "The forth letter in the alphabet is: #{colors[1]}"
+puts "The fifth letter in the alphabet is: #{colors[0]}"
+puts "The sixth letter in the alphabet is: #{colors[6]}"
+puts "The seventh letter in the alphabet is: #{colors[2]}"
